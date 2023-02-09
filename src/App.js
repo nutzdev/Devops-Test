9374644b225abd88465a1cdb1db5e3093608a2c6
dhine65 <95270275+Dhine65@users.noreply.github.com>
2023-02-09 15:18:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Team
+          Welcome nutz Team
         </p>
       </header>
     </div>
